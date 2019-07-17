1bc57eb93c9517edc3977fc7a58d8a8a3a571ac1
Mae Rine <mae.s.rine@gmail.com>
2019-07-17 04:26:03 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,13 +1,18 @@
 function getIt() {
   $('p').on('click', function(){
     alert('Hey!');
-  }
+  });
 }
 
+function frameIt() {
+  $('img').on('load',function(){
+    
+  });
+}
 
 
 $(document).ready(function(){
 
-  getit();
+  getIt();
 
 });
