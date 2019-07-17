05bae11bb6928cd37ebb9e9c54af406cf19199dd
Mae Rine <mae.s.rine@gmail.com>
2019-07-17 05:05:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,13 +1,34 @@
 function getIt() {
   $('p').on('click', function(){
     alert('Hey!');
-  }
+  });
 }
 
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass("tasty");
+  });
+}
 
+function pressIt() {
+  $('#typing').on('keydown', function(key){
+    if(key.which == 71){
+      alert('You pressed g');
+      }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  });
+}
 
 $(document).ready(function(){
 
-  getit();
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
